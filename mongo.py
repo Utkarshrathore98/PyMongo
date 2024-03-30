341beda829d11445bac9eaa8de02c8d5b4ce7c74
Utkarsh rathore <rathoreutkarsh1998@gmail.com>
2024-03-30 11:54:31 +0530

    adding insert_many functionality

--- mongo.py
+++ mongo.py
@@ -12,5 +12,22 @@ data = {
     "tags": ["mongodb", "python", "pymongo"],
     "date": datetime.datetime.now(tz=datetime.timezone.utc),
 }
-
+# Inserting one data to the collection
 app_col.insert_one(data)
+
+many_data = [
+    {
+        "author": "Urvashi",
+        "text": "Hi, I am urvashi rathore",
+        "tags": ["mongodb", "python", "pymongo"],
+        "date": datetime.datetime.now(tz=datetime.timezone.utc)
+    },
+    {
+        "author": "Prapti",
+        "text": "Hi, I am prapti rathore",
+        "tags": ["mongodb", "python", "pymongo"],
+        "date": datetime.datetime.now(tz=datetime.timezone.utc)
+    },
+]
+#Inserting many data to the collection 
+app_col.insert_many(many_data)
